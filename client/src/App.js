74b97fb8ec0816f2004f467dc331930f1d9fc713
rmcbride2121 <rmcbride2121@gmail.com>
2019-09-10 07:52:49 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import critterIcon from "./images/critter-icon.png";
 
 class App extends Component {
   constructor() {
@@ -9,8 +10,12 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        Client
+      <div className="app">
+        <div className="header">
+          <h1>critter</h1><img className="critterIcon" src={critterIcon} />
+        </div>
+        <button className="btn btn-primary">Create</button>
+        <button className="btn btn-primary">View</button>
       </div>
     );
   }
