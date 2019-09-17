08a3fdf48f7ecd14b9eeb2f7f4254d12cab5c2a2
rmcbride2121 <rmcbride2121@gmail.com>
2019-09-17 13:51:04 -0500

    added input fields to login page

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function LogIn() {
   return (
-    <div className="login">Testing</div>
+    <div className="login">
+      <input className="form-control" value="Enter Your Email Address" />
+      <input className="form-control" value="Create a Password" />
+      <button className="btn btn-primary">Meet your pet</button>
+    </div>
   );
       
 }
