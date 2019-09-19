c4750d90501cc499986819f81c6aa7bfc3dba76e
alaynapuck28 <you@example.com>
2019-09-19 18:57:55 -0500

    updates server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // require("dotenv").config();
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
@@ -7,21 +8,24 @@ const bodyParser = require("body-parser");
 
 // import mongoose from "mongoose";
 
-const userRoutes = require("./routes/userRoutes");
-const app = express();
+// const userRoutes = require("./routes/userRoutes");
 
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
 app.use(express.static("public"));
 
 // let mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://alaynapuck28:horseshoe2@cluster0-3umzc.mongodb.net/checkpoint2",
-  { useNewUrlParser: true }
-);
-app.use("/users", userRoutes);
+// mongoose.connect(
+//   "mongodb+srv://alaynapuck28:horseshoe2@cluster0-3umzc.mongodb.net/checkpoint2",
+//   { useNewUrlParser: true }
+// );
+// app.use("/users", userRoutes);
 
-mongoose.set("debug", true);
-mongoose.Promise = global.Promise;
+// mongoose.set("debug", true);
+// mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://localhost/checkpoint2");
 
 const port = process.env.PORT || 3001;
